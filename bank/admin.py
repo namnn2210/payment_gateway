498ec746a01af2b32c36445c142cf6faa40b6cd2
Ngo Ngoc Nam <namngocngo22@mgial.com>
2024-06-17 14:10:34 +0700

    edit admin

--- bank/admin.py
+++ bank/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Bank
+from .models import Bank, BankAccount
 
 # Register your models here.
 @admin.register(Bank)
@@ -7,3 +7,13 @@ class BankAdmin(admin.ModelAdmin):
     list_display = ('name', 'icon','status', 'created_at', 'updated_at')
     list_filter = ('name',)
     search_fields = ('name',)
+    
+    
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = ('user', 'account_name', 'account_number', 'balance', 'bank_name', 'username','password','bank_type', 'created_at', 'updated_at')
+    list_filter = ('bank_type', 'bank_name_id','created_at',)
+    search_fields = ('account_name', 'account_number','bank_type')
+    
+    # def user_username(self, obj):
+    #     return obj.user.username
