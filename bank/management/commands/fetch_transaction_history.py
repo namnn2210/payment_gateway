4855eb21049a309ba11855af2ce14ee8fd8da306
Ngo Ngoc Nam <namngocngo22@mgial.com>
2024-06-20 17:03:55 +0700

    edit

--- bank/management/commands/fetch_transaction_history.py
+++ bank/management/commands/fetch_transaction_history.py
@@ -7,6 +7,8 @@ from bank.database import redis_connect
 import time
 import pandas as pd
 import os
+import requests
+import re
 from datetime import datetime
 from dotenv import load_dotenv
 
@@ -15,6 +17,15 @@ load_dotenv()
 
 class Command(BaseCommand):
     help = 'Get all bank transaction history to redis'
+    
+    def find_substring(self, text):
+        # Regex pattern to find a substring starting with 'Z' and having 7 characters
+        pattern = r'Z.{6}'
+        match = re.search(pattern, text)
+        if match:
+            return match.group()
+        else:
+            return None
 
     def handle(self, *args, **kwargs):
         redis_client = redis_connect()
@@ -52,6 +63,26 @@ class Command(BaseCommand):
                             if row['type'] == 'IN':
                                 transaction_type = '+'
                                 transaction_color = '🟢'  # Green circle emoji for IN transactions
+                                
+                                order_number = self.find_substring(row['description'])
+                                if order_number:
+                                    # Create order in partner system
+                                    url = 'https://p2p.jzc899.com/be_en/ashx/control.ashx'
+                                    headers = {
+                                        'Cookie':'ASP.NET_SessionId=wrcv4xdc1uhpjm1lvhyw5fmz; cf_clearance=tOj4ybSlDyTdYrQed1U9289CWBgHUizgfc3UPHiybmg-1718865515-1.0.1.1-vIcefcRai1OePod6Kt3.Pj7OLR_v5oc8IsJdgakeT9.dmvzP1.svYa3Q09oR5wznyfd8.qcvDySrkdXzAHs0Yg'
+                                    }
+                                    data = {
+                                        'todo':'addbankorder',
+                                        'bkid':'8684',
+                                        'money':"{:.2f}".format(row['amount']),
+                                        'postscript':order_number,
+                                        'payname':'nam',
+                                        'payacctno':'3783',
+                                        'txid':''
+                                    }
+                                    response = requests.post(url=url, headers=headers, data=data)
+                                    print('===', response.json())
+                                
                             else:
                                 transaction_type = '-'
                                 transaction_color = '🔴'  # Red circle emoji for OUT transactions
@@ -64,7 +95,7 @@ class Command(BaseCommand):
                                 f'🔍 {row["type"]}\n'
                                 f'🕒 {row["active_datetime"]}'
                             )
-                            send_telegram_message(alert, os.environ.get('TRANSACTION_CHAT_ID'), os.environ.get('TRANSACTION_BOT_API_KEY'))
+                            # send_telegram_message(alert, os.environ.get('TRANSACTION_CHAT_ID'), os.environ.get('TRANSACTION_BOT_API_KEY'))
                         redis_client.set(bank.account_number, json.dumps(final_new_bank_history_df.to_dict(orient='records'), default=str))
                         print('Update for bank: %s - %s. Updated at %s' % (bank.account_number, bank.bank_name, datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
                     else:
