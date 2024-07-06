1f98908dc7983ddf6e5718f583c4ffbf9471ec22
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-07-06 16:51:19 +0700

    edit

--- payment_gateway/settings.py
+++ payment_gateway/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ru_3rxr5ze)g_t5$dkaz+*v9&gi3$2gujy*do(2d+x_1pmm5u!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['226pay.com','www.226pay.com']
 
