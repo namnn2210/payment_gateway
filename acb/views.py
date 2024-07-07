f88118ab1ac0d77991b02d4067a349aa0a0544cc
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-07-07 23:47:22 +0700

    edit

--- acb/views.py
+++ acb/views.py
@@ -20,14 +20,9 @@ def acb_login(username, password, account_number):
         "accountNo": account_number,
         "action":"login"
     }
-    response = requests.post(os.environ.get("ACB_URL"), json=body)
-    print(response.text)
-    match = re.search(r'{"refNo".*', response.text)
-    if match:
-        extracted_text = match.group(0)
-        json_response = json.loads(extracted_text)
-        if json_response['result']['ok']:
-            return True
+    response = requests.post(os.environ.get("ACB_URL"), json=body).json()
+    if response['success']:
+        return True
     
     return False
     
@@ -40,15 +35,14 @@ def acb_balance(username, password, account_number):
         "accountNo": account_number,
         "action":"balance"
     }
-    response = requests.post(os.environ.get("MB_URL"), json=body)
-    print(response.text)
-    # if response.status_code == 200:
-    #     data = response.json()
-    #     if data:
-    #         if data['result']:
-    #             if data['result']['ok']:
-    #                 acc_list = data['acct_list']
-    #                 for account in acc_list:
-    #                     if account['acctNo'] == account_number:
-    #                         return account['currentBalance']
+    response = requests.post(os.environ.get("ACB_URL"), json=body)
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            if data['result']:
+                if data['result']['ok']:
+                    acc_list = data['acct_list']
+                    for account in acc_list:
+                        if account['acctNo'] == account_number:
+                            return account['currentBalance']
     return None
