24aa1fa29f486a01b01ac3503d9d1aba0eec7a2f
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-07-07 23:41:59 +0700

    edit

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
-from acb.views import acb_login
+from acb.views import acb_login, acb_balance
 
 acb_login('0866583039','Huong1988@', '133994566668')
+acb_balance('0866583039','Huong1988@', '133994566668')
