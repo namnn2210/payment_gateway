a64014379fe9b263940ae7d6e0c0d1725472ffc6
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-07-07 20:14:44 +0700

    edit

--- mb/views.py
+++ mb/views.py
@@ -79,11 +79,12 @@ def mb_balance(username, password, account_number):
     response = requests.post(os.environ.get("MB_URL"), json=body)
     if response.status_code == 200:
         data = response.json()
-        if data['result']['ok']:
-            acc_list = data['acct_list']
-            for account in acc_list:
-                if account['acctNo'] == account_number:
-                    return account['currentBalance']
+        if data:
+            if data['result']['ok']:
+                acc_list = data['acct_list']
+                for account in acc_list:
+                    if account['acctNo'] == account_number:
+                        return account['currentBalance']
     return None
 
 def mb_transfer(request):
