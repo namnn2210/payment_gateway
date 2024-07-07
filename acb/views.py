c442c63e2887a91e77b523f634f8eee5cf30b010
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-07-07 23:41:30 +0700

    edit

--- acb/views.py
+++ acb/views.py
@@ -31,3 +31,24 @@ def acb_login(username, password, account_number):
     
     return False
     
+    
+def acb_balance(username, password, account_number):
+    body = {
+        "rows": 10,
+        "username": username,
+        "password": password,
+        "accountNo": account_number,
+        "action":"balance"
+    }
+    response = requests.post(os.environ.get("MB_URL"), json=body)
+    print(response.text)
+    # if response.status_code == 200:
+    #     data = response.json()
+    #     if data:
+    #         if data['result']:
+    #             if data['result']['ok']:
+    #                 acc_list = data['acct_list']
+    #                 for account in acc_list:
+    #                     if account['acctNo'] == account_number:
+    #                         return account['currentBalance']
+    return None
