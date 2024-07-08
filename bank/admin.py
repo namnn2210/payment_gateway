5fbcafe0502fa1afeec8c547ba4b2354ae2ab90e
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-07-08 16:19:43 +0700

    edit

--- bank/admin.py
+++ bank/admin.py
@@ -5,7 +5,7 @@ from .models import Bank, BankAccount
 # Register your models here.
 @admin.register(Bank)
 class BankAdmin(admin.ModelAdmin):
-    list_display = ('name', 'icon','status', 'created_at', 'updated_at')
+    list_display = ('id', 'name', 'icon','status', 'created_at', 'updated_at')
     list_filter = ('name',)
     search_fields = ('name',)
 
@@ -24,7 +24,7 @@ class BankAccountForm(forms.ModelForm):
 @admin.register(BankAccount)
 class BankAccountAdmin(admin.ModelAdmin):
     form = BankAccountForm
-    list_display = ('user', 'account_name', 'account_number', 'balance', 'bank_name', 'username','password', 'status', 'created_at', 'updated_at')
+    list_display = ('id', 'user', 'account_name', 'account_number', 'balance', 'bank_name', 'username','password', 'status', 'created_at', 'updated_at')
     list_filter = ('user', 'bank_type', 'bank_name_id','created_at',)
     search_fields = ('account_name', 'account_number','bank_type')
     
