1e7505daf2b8b138e7a59a64ca112d3efa4c11e1
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-07-11 14:52:12 +0700

    edit

--- payment_gateway/asgi.py
+++ payment_gateway/asgi.py
@@ -7,9 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
-import os
 
 from django.core.asgi import get_asgi_application
+django_application = get_asgi_application()
+import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -17,7 +18,6 @@ import payment_gateway.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'payment_gateway.settings')
 
-django_application = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
