143a4911764aa3c1daf2ccadd7479c7239ffc756
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-07-12 15:17:27 +0700

    edit

--- cms/models.py
+++ cms/models.py
@@ -5,7 +5,7 @@ from datetime import date
 class AmountInOut(models.Model):
     amount_in = models.IntegerField(default=0)
     amount_out = models.IntegerField(default=0)
-    date_record = models.DateField(default=date.today())
+    # date_record = models.DateField(default=date.today())
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
