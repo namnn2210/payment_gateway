d88f80d7ab1145a369068157965212068c2af849
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-07-13 11:12:20 +0700

    edit

--- partner/models.py
+++ partner/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from bank.models import BankAccount
+
+class CID(models.Model):
+    name = models.TextField()
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'cid'
 
 # Create your models here.
+class PartnerMapping(models.Model):
+    cid = models.ForeignKey(CID, on_delete=models.CASCADE, null=False)
+    key = models.TextField(null=False)
+    cardtype=models.IntegerField(null=False)
+    bank = models.ForeignKey(BankAccount, on_delete=models.CASCADE, null=False)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = 'partner_mapping'
+        
