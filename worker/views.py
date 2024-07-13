c674bf0d6e56d2fb244800e4a3f909dd14113bf8
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-07-13 14:31:05 +0700

    edit

--- worker/views.py
+++ worker/views.py
@@ -144,7 +144,7 @@ def get_transaction(bank):
                         redis_client.set(bank.account_number, json.dumps(final_new_bank_history_df.to_dict(orient='records'), default=str))
                         bank_account = BankAccount.objects.filter(account_number=str(row['account_number'])).first()
                         if bank_account:
-                            partner_mapping = PartnerMapping(bank=bank_account).first()
+                            partner_mapping = PartnerMapping.objects.filter(bank=bank_account).first()
                             if partner_mapping: 
                                 result = create_deposit_order(row)
                                 if result:
