f73ec960f2a919d76f12e5903213e4a9694a9ed8
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-07-17 23:47:16 +0700

    edit

--- vietin/views.py
+++ vietin/views.py
@@ -71,7 +71,7 @@ def vietin_transactions(username,password,account_number):
                 break
             
         for item in fetch_transactions:
-            if item['channel'] == '44 - Gateway (VTB)':
+            if 'CT DEN' in item['remark']:
                 transaction_type='IN'
             else:
                 transaction_type='OUT'
