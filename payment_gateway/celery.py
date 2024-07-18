e69ea80ac9e701d720955d7896ed9d6699d068dd
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-07-19 00:51:30 +0700

    edit

--- payment_gateway/celery.py
+++ payment_gateway/celery.py
@@ -1,21 +1,22 @@
-# myproject/celery.py
-
-from __future__ import absolute_import, unicode_literals
 import os
+
 from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
+# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'payment_gateway.settings')
 
-app = Celery('payment_celery')
+app = Celery('payment_gateway')
 
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
-# app.config_from_object('django.conf:settings', namespace='CELERY')
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Load task modules from all registered Django app configs.
+# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
-@app.task(bind=True)
+
+@app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
