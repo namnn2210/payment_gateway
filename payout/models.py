a64d35b8d1596eac7b4538ac42059ee233df8a80
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-07-27 11:28:15 +0700

    edit

--- payout/models.py
+++ payout/models.py
@@ -5,7 +5,7 @@ from bank.models import Bank
 # Create your models here.
 class Payout(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    did = models.CharField(max_length=255, blank=True)
+    did = models.CharField(max_length=255, default='')
     scode = models.CharField(max_length=255)
     orderno = models.CharField(max_length=255, null=True, default='')
     orderid = models.CharField(max_length=255)
@@ -13,14 +13,14 @@ class Payout(models.Model):
     bankname = models.CharField(max_length=255, null=True)
     accountno = models.CharField(max_length=255)
     accountname = models.CharField(max_length=255)
-    bankcode = models.CharField(max_length=255)
-    partner_bankcode = models.CharField(max_length=10, null=True)
+    bankcode = models.CharField(max_length=255, default='')
+    partner_bankcode = models.CharField(max_length=10, default='')
     is_auto = models.BooleanField(default=False)
     is_cancel = models.BooleanField(default=False)
     is_report = models.BooleanField(default=False)
-    process_bank = models.ForeignKey(Bank, on_delete=models.CASCADE, null=True, blank=False)
+    process_bank = models.ForeignKey(Bank, on_delete=models.CASCADE, default='')
     status = models.BooleanField(default=False,null=True)
-    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='payout_created_by', null=True, blank=False)
+    updated_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='payout_created_by', default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
