20f76416d70f2d68053f4f2c77d825569e5e2895
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-08-06 21:10:18 +0700

    edit

--- employee/admin.py
+++ employee/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import EmployeeDeposit
 # Register your models here.
+
+@admin.register(EmployeeDeposit)
+class EmployeeDepositAdmin(admin.ModelAdmin):
+    list_display = ('user', 'amount', 'bankname','accountno','accountname','note','bankcode','status', 'created_at','updated_by', 'updated_at')
+    list_filter = ('user','bankname','bankcode',)
+    search_fields = ('name','bankname','bankcode',)
