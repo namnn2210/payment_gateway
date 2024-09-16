a6e29b0efbaa6df91fe2ada702129e27f3c064d2
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-09-16 23:56:59 +0700

    edit

--- employee/models.py
+++ employee/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from datetime import datetime
+import pytz
 
 # Create your models here.
 class EmployeeDeposit(models.Model):
@@ -22,7 +24,7 @@ class EmployeeDeposit(models.Model):
 
 class EmployeeWorkingSession(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=False)
-    start_time = models.DateTimeField(default=timezone.now, null=False)
+    start_time = models.DateTimeField(default=datetime.now(pytz.timezone('Asia/Bangkok')), null=False)
     start_balance = models.BigIntegerField(default=0)
     end_time = models.DateTimeField(null=True)
     end_balance = models.BigIntegerField(default=0)
