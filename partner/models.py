eec42f40571985467f2aca7c0fb19d40a1142144
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-09-24 13:49:52 +0700

    edit cid

--- partner/models.py
+++ partner/models.py
@@ -3,6 +3,8 @@ from bank.models import BankAccount
 
 class CID(models.Model):
     name = models.TextField()
+    key = models.TextField(null=True)
+    cardtype = models.IntegerField(null=True)
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
