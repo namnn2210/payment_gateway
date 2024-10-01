8693ae43b6d7aeeff63f0bd0a5bf9eaa0daefbf0
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-10-01 10:57:29 +0700

    edit

--- mb/views.py
+++ mb/views.py
@@ -143,12 +143,12 @@ def mb_webhook(request):
                 for _, row in new_transaction_df.iterrows():
                     history = json.loads(redis_client.get(new_formatted_transaction.account_number))
                     transaction_exists = False
-                    for transaction in history:
-                        if transaction['transaction_number'] == row['transaction_number']:
-                            transaction_exists = True
-                            break
-                    if transaction_exists:
-                        continue
+                    # for transaction in history:
+                    #     if transaction['transaction_number'] == row['transaction_number']:
+                    #         transaction_exists = True
+                    #         break
+                    # if transaction_exists:
+                    #     continue
                     if not datetime.strptime(row["transaction_date"],
                                              '%d/%m/%Y %H:%M:%S').date() >= timezone.now().date():
                         continue
