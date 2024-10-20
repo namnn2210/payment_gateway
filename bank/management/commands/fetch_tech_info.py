c7199452d90daa899b4e1b38a26cca6474f09660
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-10-21 06:42:44 +0700

    edit

--- bank/management/commands/fetch_tech_info.py
+++ bank/management/commands/fetch_tech_info.py
@@ -18,15 +18,15 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         while True:
             # Get all active bank accounts
-            try:
-                bank_accounts = BankAccount.objects.filter(bank_name=3,status=True)
-                for bank in bank_accounts:
-                    get_balance(bank=bank)
-                time.sleep(15)
-            except Exception as ex:
-                alert = (
-                    f'🔴 - SYSTEM ALERT\n'
-                    f'Fetch Techcombank bank info error: {str(ex)}\n'
-                    f'Date: {datetime.now(pytz.timezone('Asia/Bangkok'))}'
-                )
-                send_telegram_message(alert, os.environ.get('MONITORING_CHAT_ID'), os.environ.get('MONITORING_BOT_API_KEY'))
+            # try:
+            bank_accounts = BankAccount.objects.filter(bank_name=3,status=True)
+            for bank in bank_accounts:
+                get_balance(bank=bank)
+            time.sleep(15)
+            # except Exception as ex:
+            #     alert = (
+            #         f'🔴 - SYSTEM ALERT\n'
+            #         f'Fetch Techcombank bank info error: {str(ex)}\n'
+            #         f'Date: {datetime.now(pytz.timezone('Asia/Bangkok'))}'
+            #     )
+            #     send_telegram_message(alert, os.environ.get('MONITORING_CHAT_ID'), os.environ.get('MONITORING_BOT_API_KEY'))
