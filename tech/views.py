0787e9f7c9767554a9b468a5d13d5611459d35fe
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-10-20 18:40:44 +0700

    edit

--- tech/views.py
+++ tech/views.py
@@ -56,6 +56,7 @@ def tech_transactions(username, password, account_number):
         transactions = response['transactions']
         transaction_type = ''
         for transaction in transactions:
+            print(transaction)
             if 'category' not in transaction.keys():
                 continue
             if transaction['category'] == "Spending":
