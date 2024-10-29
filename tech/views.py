436b91dee1aade00d903afdbd15b2f98d4a14696
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-10-29 14:22:53 +0700

    edit

--- tech/views.py
+++ tech/views.py
@@ -32,10 +32,10 @@ def tech_balance(username, password, account_number):
                 print(item['availableBalance'])
                 return item['availableBalance']
     if type(response) == dict :
-        print(response)
-        if response['message'] == 'Request failed with status code 401':
+
+        if 'message' in response.keys() and response['message'] == 'Request failed with status code 401':
             return None
-        if not response['success']:
+        if 'success' in response.keys() and not response['success']:
             return None
     return None
 
