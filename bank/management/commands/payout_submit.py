3bde67bfae16f4b81c0e309706da9068cff280de
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-12-20 09:16:51 +0700

    edit

--- bank/management/commands/payout_submit.py
+++ bank/management/commands/payout_submit.py
@@ -18,11 +18,17 @@ class Command(BaseCommand):
             pending_payouts = Payout.objects.filter(status=False, partner_status=False)
             for payout in pending_payouts:
                 try:
-                    sleep_time = random.randint(1,10)
+                    sleep_time = random.randint(1,7)
                     time.sleep(sleep_time)
                     if update_status_request(payout=payout, status='S'):
                         payout.partner_status = True
                         payout.save()
+                        alert = (
+                            f'🔴 - THÔNG BÁO PAYOUT\n'
+                            f'Đã có lệnh payout mới. Vui lòng kiểm tra và hoàn thành !!"\n'
+                        )
+                        send_telegram_message(alert, get_env('PENDING_PAYOUT_CHAT_ID'),
+                                              get_env('MONITORING_BOT_API_KEY'))
                 except Exception as ex:
                     alert = (
                         f'🔴 - SYSTEM ALERT\n'
