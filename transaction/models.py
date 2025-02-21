8e8496e464a87997f18bec465c76b7db92ebceba
Ngo Ngoc Nam <namngocngo22@gmail.com>
2025-02-21 09:50:57 +0700

    edit

--- transaction/models.py
+++ transaction/models.py
@@ -13,7 +13,7 @@ class TransactionHistory(models.Model):
     status = models.CharField(max_length=255, null=True)
     transaction_type = models.CharField(max_length=255,null=False)
     transaction_number = models.CharField(max_length=255, null=False, default='')
-    transfer_code = models.CharField(max_length=255, null=True, unique=True)
+    transfer_code = models.CharField(max_length=255, null=True)
     created_at = models.DateTimeField(auto_now_add=True, null=False)
     updated_at = models.DateTimeField(auto_now=True)
 
