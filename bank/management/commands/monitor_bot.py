7a0e671076ed7b9b7e03a8bdec4b91efce772b15
namngocngo22 <nathan.ng@sparkgo.ai>
2025-07-04 12:03:36 +0700

    edit

--- bank/management/commands/monitor_bot.py
+++ bank/management/commands/monitor_bot.py
@@ -8,10 +8,13 @@ from django.contrib.auth.models import User
 from employee.models import EmployeeWorkingSession
 from asgiref.sync import sync_to_async
 
+from asgiref.sync import sync_to_async
+from django.utils import timezone
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     args = context.args
 
-    if not args:
+    if len(args) < 2:
         await update.message.reply_text(
             "Nhập thông tin sai. Mẫu: start username số dư hiện tại\nVí dụ:\n/start admin 12345"
         )
@@ -19,31 +22,40 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     username = args[0]
     if username.isdigit() or username.isdecimal():
-         await update.message.reply_text(
-            f"Sai tên đăng nhập"
-        )
+        await update.message.reply_text("Sai tên đăng nhập")
+        return
 
     start_balance = args[1]
     if not start_balance.isdigit():
-        await update.message.reply_text(
-            f"Sai số dư "
-        )
+        await update.message.reply_text("Sai số dư")
         return
-    
+
+    start_balance_int = int(start_balance)
+
     employee = await sync_to_async(lambda: User.objects.filter(username=username).first())()
-    undone_session = await sync_to_async(lambda:EmployeeWorkingSession.objects.filter(user=employee, status=False).first())()
+    if not employee:
+        await update.message.reply_text(f"Không tìm thấy nhân viên có username '{username}'.")
+        return
+
+    undone_session = await sync_to_async(
+        lambda: EmployeeWorkingSession.objects.filter(user=employee, status=False).first()
+    )()
+
     if undone_session:
-            update.message.reply_text('Bạn đang trong phiên làm việc. Không thể bắt đầu')
-    else:
-        start_time = timezone.now()
-        EmployeeWorkingSession.objects.create(
-            user=employee,
-            start_time=start_time,  
-            start_balance=start_balance
-        )
-        await update.message.reply_text(
-            f"Nhân viên: {employee.username}\nGiờ bắt đầu: {start_time}\n Số dư bắt đầu: {start_balance}\n"
-        )
+        await update.message.reply_text('Bạn đang trong phiên làm việc. Không thể bắt đầu')
+        return
+
+    
+    start_time = timezone.now()
+    await sync_to_async(EmployeeWorkingSession.objects.create)(
+        user=employee,
+        start_time=start_time,
+        start_balance=start_balance_int
+    )
+
+    await update.message.reply_text(
+        f"Nhân viên: {employee.username}\nGiờ bắt đầu: {start_time.strftime('%Y-%m-%d %H:%M:%S')}\nSố dư bắt đầu: {start_balance_int}"
+    )
 
 class Command(BaseCommand):
     help = 'Session Bot Management'
