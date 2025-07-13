d5a5e5e20770845f20f256b7595ee59097b5396b
namngocngo22 <nathan.ng@sparkgo.ai>
2025-07-13 19:01:00 +0700

    edit

--- mb/views.py
+++ mb/views.py
@@ -19,7 +19,7 @@ def mb_login(username, password, account_number):
     print('start login: ', datetime.now())
     response = requests.post(f'{get_env("MBB_URL")}/login', json=body, timeout=120)
     print(response.text)
-    if '"ok":true' in response.text:
+    if '"success":true' in response.text:
         print('end login: ', datetime.now())
         return True
     return False
