5cb2991fa4c5a61bc49dc538e473c3aa0dcf4d34
namngocngo22 <nathan.ng@sparkgo.ai>
2025-07-24 10:10:14 +0700

    edit

--- transaction/admin.py
+++ transaction/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import TransactionHistory
 
 # Register your models here.
+@admin.register(TransactionHistory)
+class TransactionHistoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'account_number', 'amount','description','incomingorderid','note','orderid','scode','transaction_type','transaction_number','transfer_code','payername','status', 'created_at', 'updated_at')
+    list_filter = ('account_number',)
+    search_fields = ('account_number','description',)
