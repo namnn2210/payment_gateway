f61311e9695d34e29faad0f50c712ca26e32e82a
namngocngo22 <nathan.ng@sparkgo.ai>
2025-07-24 10:06:02 +0700

    edit

--- bank/management/commands/fetch_acb_info.py
+++ bank/management/commands/fetch_acb_info.py
@@ -16,5 +16,17 @@ class Command(BaseCommand):
             # Get all active bank accounts
             bank_accounts = BankAccount.objects.filter(bank_name=2, status=True)
             for bank in bank_accounts:
-                get_balance(bank=bank)
+                try:
+                    get_balance(bank=bank)
+                except Exception as ex:
+                    alert = (
+                        f'🔴 - SYSTEM ALERT\n'
+                        f'Fetch ACB bank info error: {str(ex)}\n'
+                        f'Date: {datetime.now(pytz.timezone('Asia/Singapore'))}'
+                    )
+                    try:
+                        send_telegram_message(alert, get_env('MONITORING_CHAT_ID'),
+                                            get_env('MONITORING_BOT_2_API_KEY'))
+                    except Exception as ex:
+                        print(str(ex))
             time.sleep(15)
