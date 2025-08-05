ac86d81a30d2284c3864d06a383bb5c6305a5e54
namngocngo22 <nathan.ng@sparkgo.ai>
2025-08-05 11:57:29 +0700

    edit

--- payout/views.py
+++ payout/views.py
@@ -347,7 +347,7 @@ class PayoutWebhookAPIView(APIView):
                 f'- - - - - - - - - - - - - -\n'
             )
             memo = 'TQ' + orderno[-11:]
-            img_url = f'https://img.vietqr.io/image/{system_bankcode}-{accountno}-compact.jpg?amount={int(float(money))}&addInfo={accountname}&accountName={accountname}'
+            img_url = f'https://img.vietqr.io/image/{system_bankcode}-{accountno}-compact.jpg?amount={int(float(money))}&addInfo={accountname.upper()}&accountName={accountname}'
             send_telegram_message(alert, get_env('PENDING_PAYOUT_CHAT_ID'), get_env('MONITORING_BOT_2_API_KEY'))
             send_telegram_qr(get_env('MONITORING_BOT_2_API_KEY'), '-1002888070097', img_url, caption)
         else:
@@ -398,7 +398,7 @@ class PayoutWebhookAPIView(APIView):
                 f'{int(float(money)):,}\n'
                 f'- - - - - - - - - - - - - -\n'
             )
-            img_url = f'https://img.vietqr.io/image/{system_bankcode}-{accountno}-compact.jpg?amount={int(float(money))}&addInfo={accountname}&accountName={accountname}'
+            img_url = f'https://img.vietqr.io/image/{system_bankcode}-{accountno}-compact.jpg?amount={int(float(money))}&addInfo={accountname.upper()}&accountName={accountname}'
             send_telegram_message(alert, get_env('PENDING_PAYOUT_CHAT_ID'),
                                   get_env('MONITORING_BOT_2_API_KEY'))
             send_telegram_qr(get_env('MONITORING_BOT_2_API_KEY'), '-1002287492730', img_url, caption)
